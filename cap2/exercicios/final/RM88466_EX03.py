c078cb8141640ce48357fa3070bc321c93c815a1
Paulo <paulodigitalart@gmail.com>
2021-04-01 19:47:57 -0300

    up

--- cap2/exercicios/final/RM88466_EX03.py
+++ cap2/exercicios/final/RM88466_EX03.py
@@ -1,28 +1,42 @@
-segundaFeira = int(input("Digite os votos para Segunda-feira"))
-tercaFeira = int(input("Digite os votos para Terça-feira"))
-quartaFeira = int(input("Digite os votos para Quarta-feira"))
-quintaFeira = int(input("Digite os votos para Quinta-feira"))
-sextaFeira = int(input("Digite os votos para Sexta-feira"))
+segundaFeira = int(input("\033[0;0m"+"Digite os votos para Segunda-feira: "))
+tercaFeira = int(input("Digite os votos para Terça-feira: "))
+quartaFeira = int(input("Digite os votos para Quarta-feira: "))
+quintaFeira = int(input("Digite os votos para Quinta-feira: "))
+sextaFeira = int(input("Digite os votos para Sexta-feira: "))
 
 if segundaFeira > tercaFeira and segundaFeira > quartaFeira:
-    print("Mais votado: Segunda-feira", segundaFeira)
     maisVotado = segundaFeira
     if segundaFeira > quartaFeira and segundaFeira > sextaFeira:
+        print("====================================")
+        print("Mais votado: Segunda-feira", "\033[31m", segundaFeira, "votos")
         maisVotado = segundaFeira
-        print("Mais votado: Segunda-feira", segundaFeira)
-elif tercaFeira > segundaFeira and tercaFeira > quartaFeira:
-    print("Mais votado: Terca-feira", tercaFeira)
+
+if tercaFeira > segundaFeira and tercaFeira > quartaFeira:
     maisVotado = tercaFeira
     if tercaFeira > quintaFeira and tercaFeira > sextaFeira:
+        print("====================================")
+        print("Mais votado: Terca-feira", "\033[31m", tercaFeira, "votos")
         maisVotado = tercaFeira
-        print("Maior", tercaFeira)
-elif quartaFeira > segundaFeira and quartaFeira > tercaFeira:
-    print("Mais votado: Quarta-feira", quartaFeira)
+if quartaFeira > segundaFeira and quartaFeira > tercaFeira:
     maisVotado = quartaFeira
     if quartaFeira > quintaFeira and quartaFeira > sextaFeira:
-        print("Mais votado: Quarta-feira", quartaFeira)
+        print("====================================")
+        print("Mais votado: Quarta-feira", "\033[31m", quartaFeira, "votos")
         maisVotado = quartaFeira
 
+if quintaFeira > segundaFeira and quintaFeira > tercaFeira:
+    maisVotado = quartaFeira
+    if quintaFeira > quartaFeira and quintaFeira > sextaFeira:
+        print("====================================")
+        print("Mais votado: Quinta-feira", "\033[31m", quintaFeira, "votos")
+        maisVotado = quartaFeira
+
+if sextaFeira > segundaFeira and sextaFeira > tercaFeira:
+    maisVotado = sextaFeira
+    if sextaFeira > quartaFeira and sextaFeira > quintaFeira:
+        print("====================================")
+        print("mais votado: Sexta", "\033[31m", sextaFeira, "votos")
+        maisVotado = sextaFeira
+
 total = segundaFeira + tercaFeira + quartaFeira + quintaFeira + sextaFeira
-print("Maid votado:", maisVotado)
-print("Total de votos", total)
+print("\033[30m"+"\033[47m"+"Total de votos"+"\033[0;0m"+"\033[31m", total)
