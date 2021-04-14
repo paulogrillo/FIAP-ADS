ab98dc3a29e8b6b1d867da4af800680368e6b8c3
Paulo <paulodigitalart@gmail.com>
2021-04-13 23:47:39 -0300

    up

--- cap3/exercicios/RM88466_EX03.py
+++ cap3/exercicios/RM88466_EX03.py
@@ -1,18 +1,18 @@
-#3 – Uma grande empresa de jogos está querendo tornar seus games mais desafiadores. Por isso ela contratou você para desenvolver um algoritmo que será aplicado futuramente em diversos outros games: o algoritmo da sorte de Fibonnaci.
+# 3 – Uma grande empresa de jogos está querendo tornar seus games mais desafiadores. Por isso ela contratou você para desenvolver um algoritmo que será aplicado futuramente em diversos outros games: o algoritmo da sorte de Fibonnaci.
 
-#A ideia dessa empresa, é claro, é fazer com que seja mais difícil os jogadores terem sucesso nas ações que realizam nos games. 
+# A ideia dessa empresa, é claro, é fazer com que seja mais difícil os jogadores terem sucesso nas ações que realizam nos games.
 
-#Por isso o seu algoritmo deverá funcionar da seguinte forma:
+# Por isso o seu algoritmo deverá funcionar da seguinte forma:
 
-#O usuário deve digitar um valor numérico inteiro e o algoritmo deverá verificar se esse valor encontra-se na sequência de Fibonnaci.
+# O usuário deve digitar um valor numérico inteiro e o algoritmo deverá verificar se esse valor encontra-se na sequência de Fibonnaci.
 
-#Caso o número esteja na sequência, o algoritmo deve exibir a mensagem “Ação bem sucedida!” e, caso não esteja, deve exibir a mensagem “A ação falhou...”
+# Caso o número esteja na sequência, o algoritmo deve exibir a mensagem “Ação bem sucedida!” e, caso não esteja, deve exibir a mensagem “A ação falhou...”
 
-#A sequência de Fibonacci é muito simples e possui uma lógica de fácil compreensão: ela se inicia em 1 e cada novo elemento da sequência é a soma entre os dois elementos anteriores. Portanto: 1, 1, 2, 3, 5, 8, 13, 21, e assim por diante.
+# A sequência de Fibonacci é muito simples e possui uma lógica de fácil compreensão: ela se inicia em 1 e cada novo elemento da sequência é a soma entre os dois elementos anteriores. Portanto: 1, 1, 2, 3, 5, 8, 13, 21, e assim por diante.
 
-#Logo, se o usuário digitar o número 55 o programa deverá informar que a ação foi bem sucedida, afinal 55 está entre os números da sequência.
+# Logo, se o usuário digitar o número 55 o programa deverá informar que a ação foi bem sucedida, afinal 55 está entre os números da sequência.
 
-#Mas se o usuário digitar o número 6, por exemplo, a ação não será bem sucedida, pois o número 6 não está na sequência.
+# Mas se o usuário digitar o número 6, por exemplo, a ação não será bem sucedida, pois o número 6 não está na sequência.
 
 # Program to display the Fibonacci sequence up to n-th term
 
@@ -28,8 +28,7 @@ while i > n3:
     n2 = n3
 if i == 0:
     print("O número 0 está na sequência de fibonacci. \nO Próximo número é: 1")
+elif i == n3:
+    print("O número", i, "está na sequência de fibonacci!")
 else:
-    if i == n3:
-        print("O número", i, "está na sequência de fibonacci!")
-    else:
-        print("Este número não faz parte da sequência de fibonacci!")
+    print("Este número não faz parte da sequência de fibonacci!")
